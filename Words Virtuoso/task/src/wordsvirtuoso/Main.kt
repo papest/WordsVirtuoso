d4982a2e15ce58d642c18486fb43137e1790a64e
Epap <peia@inbox.ru>
2023-05-08 12:43:36 +0300

    Stage 2/6: Read words from a file

--- Words Virtuoso/task/src/wordsvirtuoso/Main.kt
+++ Words Virtuoso/task/src/wordsvirtuoso/Main.kt
@@ -1,20 +1,36 @@
 package wordsvirtuoso
 
+import java.io.File
+import java.util.*
+
 fun String.answer(): String {
     println(this)
     return readln()
 }
 
 const val WORD_SIZE = 5
+
+fun validWord(word: String, wordSize: Int = WORD_SIZE): Boolean {
+    val word = word.trim()
+    val regex = ("\\b[a-z]{$wordSize}\\b").toRegex(RegexOption.IGNORE_CASE)
+    return word.lowercase().toSet().size == wordSize && regex.matches(word)
+}
+
 fun main() {
-    val regex = "^[a-zA-Z]{5}$".toRegex(RegexOption.IGNORE_CASE)
-    val str = "Input a 5-letter string:".answer().trim()
-    println(
-        when {
-            str.length != WORD_SIZE -> "The input isn't a $WORD_SIZE-letter string."
-            str.lowercase().toSet().size < WORD_SIZE -> "The input has duplicate letters."
-            !regex.matches(str) -> "The input has invalid characters."
-            else -> "The input is a valid string."
+    val fileName = "Input the words file:".answer()
+    val file = File(fileName)
+    var invalidWordsCount = 0
+    try {
+        val scanner = Scanner(file)
+        while (scanner.hasNext()) {
+            val word = scanner.nextLine()
+            if (!validWord(word)) invalidWordsCount++
         }
-    )
+        println(
+            if (invalidWordsCount == 0) "All words are valid!"
+            else "Warning: $invalidWordsCount invalid words were found in the $fileName file."
+        )
+    } catch (e: Exception) {
+        println("Error: The words file $fileName doesn't exist.")
+    }
 }
