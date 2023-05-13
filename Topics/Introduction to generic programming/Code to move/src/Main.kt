f23ec4841242dae9ba2f395baac04f278822fc22
Epap <peia@inbox.ru>
2023-05-13 18:09:16 +0300

    Generic programming, primary constructor

---
+++ Topics/Introduction to generic programming/Code to move/src/Main.kt
@@ -0,0 +1,7 @@
+class Box<T>(private var furnitureFromBox: T, private var boxVolume: Int) {
+    fun getBoxVolume() = boxVolume
+    fun getFurnitureFromBox() = furnitureFromBox
+}
+
+class Fridge
+class Armchair
