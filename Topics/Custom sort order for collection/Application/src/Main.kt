8585fd885d225c434f3e2842c084b0361d5340d6
Epap <peia@inbox.ru>
2023-05-30 11:47:59 +0300

    Ordering elements in collection

---
+++ Topics/Custom sort order for collection/Application/src/Main.kt
@@ -0,0 +1,8 @@
+data class Ship(val name: String, val ammunition: Int)
+
+fun main() {
+    val ships = readln().split(" ")
+    val shipsList = ships.map { Ship(it.split("-")[0], it.split("-")[1].toInt()) }
+    val res = shipsList.sortedBy { it.ammunition }
+    println(res)
+}
---
+++ Topics/Ordering elements in collection/List of students/src/Main.kt
@@ -0,0 +1,4 @@
+fun main() {
+    val list = readln().split(" ")
+    println(list.sorted())
+}
