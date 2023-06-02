ba078aa191d9266412d3704ae77ea82a4000c90b
Epap <peia@inbox.ru>
2023-06-02 08:32:30 +0300

    Function reference

---
+++ Topics/Function references/Calculator/src/Main.kt
@@ -0,0 +1,25 @@
+fun add(x: Int, y: Int) = x + y
+
+fun subtract(x: Int, y: Int) = x - y
+
+fun divide(x: Int, y: Int) = x / y
+
+fun multiply(x: Int, y: Int) = x * y
+
+fun calculate(x: Int, y: Int, operator: (Int, Int) -> Int) {
+    val result = operator(x, y)
+    print(result)
+}
+
+fun main() {
+    val x: Int = readLine()!!.toInt()
+    val y: Int = readLine()!!.toInt()
+    val operator: String = readLine()!!
+    when (operator) {
+
+        "add" -> calculate(x, y, ::add)
+        "subtract" -> calculate(x, y, ::subtract)
+        "multiply" -> calculate(x, y, ::multiply)
+        "divide" -> calculate(x, y, ::divide)
+    }
+}
